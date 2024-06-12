da347d492736c3287b838cc01d25402612f7907a
kyriakidisdimitrios <kyriakidis.demetrios@gmail.com>
2024-06-12 04:57:48 +0100

    feat: fresh project-1 code

--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
+#include <climits>
 using namespace std;
-int main() {
-    cout<<"enter 2 numbers";
-    int a,b,c; //ta string dhlwnonte string
-    cin>>a>>b;
-    //getline(cin,a); //epeidi alliws pernei oti yparxei prin to keno, auto pernei thn protash mexri na xtyph8ei to ENTER
-    c= a+b;
-    cout<<"Addition is"<< c;
-    //std::cout << "Hello, World!" << std::endl; //to cout shmainei console.out to :: legetai scope resolution
-    return 0; //
 
+    //char x=128; bgazei -128, paei kuklika
+
+enum day {mon=1, tue,wed,thy,fri,sat,sun};
+int main() {
+    day d;
+    d=mon;
+    cout<<tue<<endl;
+
+    int min=INT_MIN;
+    int A[10]= {-2,-4,-6,-8,-12};
+    for (auto x:A) {
+        if (x>min)
+            min = x; //mallon anapoda ennoei
+    }
+    cout<<"Maximum number is: ";
+    cout<<min<<endl; //fernei 0, giati einai 10thesio kai ta kena einai 0
+    cout<<INT_MIN;
+    return 0;
 }
