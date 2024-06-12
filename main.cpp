321962dcea1ad22cae7649812b842b3a3c0bb834
kyriakidisdimitrios <kyriakidis.demetrios@gmail.com>
2024-06-12 02:38:16 +0100

    feat: fresh project-1 code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl; //to cout shmainei console.out to :: legetai scope resolution
+    cout<<"enter 2 numbers";
+    int a,b,c; //ta string dhlwnonte string
+    cin>>a>>b;
+    //getline(cin,a); //epeidi alliws pernei oti yparxei prin to keno, auto pernei thn protash mexri na xtyph8ei to ENTER
+    c= a+b;
+    cout<<"Addition is"<< c;
+    //std::cout << "Hello, World!" << std::endl; //to cout shmainei console.out to :: legetai scope resolution
     return 0; //
 
 }
