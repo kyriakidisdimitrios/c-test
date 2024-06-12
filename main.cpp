5218c3e00c7bd783de94c29137d2e27da3f5378f
kyriakidisdimitrios <kyriakidis.demetrios@gmail.com>
2024-06-12 11:45:39 +0100

    feat: fresh project-1 code

--- main.cpp
+++ main.cpp
@@ -3,10 +3,40 @@
 using namespace std;
 
 int main() {
-    int a = 10;
-    int *p= &a;
-    cout<<*p<<endl;
 
+    int x=10;
+    int &y =x;
+    cout<<x<<endl;
+    y++;
+    x++;
+    cout<<x<<endl;
+
+    // int a = 10;
+    // int *p= &a;
+    // cout<<*p<<endl;
+
+    // int *p = new int[5];
+    // p[0]=12;
+    // p[1]=13;
+    // cout<<p[1]<<endl;
+    //
+    // delete []p;
+    // p=nullptr;
+
+    // int size;
+    // cout<<"Enter number of size";
+    // cin>>size;
+    // int A[size];
+    // cout<<sizeof A <<endl;
+
+    // int size;
+    // cout<<"Enter number of size";
+    // cin>>size;
+    // int *p = new int[size];
+    //
+    // cout<<"Enter number of size";
+    // cin>>size;
+    // p = new int[size];
 
 
     return 0;
