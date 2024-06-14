d1c12dc68fdc72f7117dc018f2a27e424f733eb9
kyriakidisdimitrios <kyriakidis.demetrios@gmail.com>
2024-06-14 10:16:06 +0100

    feat: fresh project-1 code

--- main.cpp
+++ main.cpp
@@ -5,9 +5,74 @@ using namespace std;
 
 int main() {
 
-    char s1[20] ="Porgramming";
 
-    cout<<strchr(s1,'g')<<endl;
+    string str ="john123@gmail.com";
+    int i= str.find('@');
+    cout<<str.substr(0,i);
+
+    // int count=0;
+    // string str = "madam";
+    // string str2 = "";
+    // int len = (int)str.length();
+    // str2.resize(len);
+    //
+    // for (int i=0,j=len-1; i<len; i++,j--) {
+    //
+    //     str2[i] =str[j];
+    // }
+    // if(str.compare(str2)==0) {
+    //     cout<<"it is a palindrome";
+    // }
+
+    // for (int i=0;str[i]!='\0';i++) {
+    //     if (str[i]>=65 && str[i]<90)
+    //         str[i] =str[i] + 32;
+    // }
+    // cout<<str;
+
+    // string::iterator it;
+    // for(it=str.begin();it!=str.end();it++)
+    //     count++;
+    // cout<<count;
+
+    // for(int i=0; str[i]!='\0'; i++) {
+    //     count++;
+    // }
+
+
+
+    // string str ="today";
+    // string::iterator it;
+    // for (it=str.begin(); it!=str.end();it++) {
+    //     *it=*it-32; //ta kanei kefalaia ena ena
+    //     //cout<<*it;
+    // }
+    // string str ="today";
+    // string::reverse_iterator it; //auto kai to apo katw kanei anapoda
+    // for (it=str.rbegin(); it!=str.rend();it++) {
+    //     //*it=*it-32; //ta kanei kefalaia ena ena
+    //     cout<<*it;
+    // }
+    // //cout<<str;
+    // cout<<endl;
+    // string str= "Programming";
+    //
+    // str[4]='M';
+    // cout<<str<<endl;
+    // string str= "Hello World";
+    // cout<<str.find_first_of("le");
+
+
+    // char s1[10]="235";
+    // char s2[10]="54.78";
+    //
+    //
+    // long int x=strtol(s1,NULL, 10);
+    // float y =strtof(s2,NULL);
+    //
+    // cout<<x+10<<endl<<y<<endl;
+    // char s1[20] ="Porgramming";
+    // cout<<strchr(s1,'g')<<endl;
 
 
     // char s1[20] ="Good";
