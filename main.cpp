146fdbea874f3d9692b2e2b2de7413a0d46c4793
kyriakidisdimitrios <kyriakidis.demetrios@gmail.com>
2024-06-15 03:54:08 +0100

    feat: fresh project-1 code

--- main.cpp
+++ main.cpp
@@ -3,41 +3,29 @@
 #include <string.h>
 using namespace std;
 
+class Rectangle {
+public:
+    int length;
+    int breadth;
 
-int * fun() {
-    int *p = new int[5]; //edw einai katanemhmeno sto heap
-    for (int i=0;i<5;i++) {
-        p[i]=5*i;
+    int area() {
+        return length*breadth;
     }
-    return p;
+    int perimeter() {
+        return 2*(length*breadth);
+    }
+};
 
-    // int x=10; // auto den mporei na epistrafei giati den einai heap
-    // return &x;
-
-}
-// void swap(int *a, int *b) {
-//     int temp;
-//     temp=*b;
-//     *b=*a;
-//     *a=temp;
-// }
-// void swap(int &a, int &b) {
-//     int temp;
-//     temp = a;
-//     a=b;
-//     b=temp;
-// }
 
 int main() {
-    int *q=fun();
-    for (int i=0;i<5;i++) {
-        cout<<q;
-    }
-    // int x=10, y=20;
-    // //swap(&x, &y);
-    // swap(x, y);
-    // cout<<x<<y;
+    Rectangle r1;
+    Rectangle *ptr;
+    //ptr = &r1; //auto to bazei ston stack
+    ptr = new Rectangle; //auto to bazei sto heap
+    ptr->length=10;
+    ptr->breadth=5;
+    cout<<ptr->area()<<endl;
+    cout<<ptr->perimeter()<<endl;
 
-
-return 0;
+    return 0;
 }
