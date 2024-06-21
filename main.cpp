dbaafb55200aae40fc0e9df91904947ffc6c803f
kyriakidisdimitrios <kyriakidis.demetrios@gmail.com>
2024-06-21 15:07:35 +0100

    feat: fresh project-1 code

--- main.cpp
+++ main.cpp
@@ -5,29 +5,56 @@ using namespace std;
 
 class Base {
 public:
-    void fun1() {
-        cout<<"fun1 of base"<<endl;
+    //void fun() {
+    virtual void fun() {
+        cout<<"fun of base"<<endl;
     }
 };
 class Derived: public Base {
-    public:
-    void fun2() {
-        cout<<"fun2 of derived"<<endl;
+public:
+    void fun() {
+        cout<<"fun of derived"<<endl;
     }
 };
 int main() {
 
-    Derived d; //fun1 of base fun2 of derived
-    d.fun1();
-    d.fun2();
+    Derived d;
+    //d.fun();
 
-    Derived d1; //fun1 of base fun2 of derived
-    Base *ptr = &d1;
-    ptr->fun1();
-    //ptr->fun2(); //den to blepei
+    //Base *ptr = &d;
+    Base *ptr = new Derived();
+    ptr->fun();
 
-    Base b;
-    //Derived *ptr2 = &b; //den epitepetai
 
     return 0;
 }
+
+
+// class Base {
+// public:
+//     void fun1() {
+//         cout<<"fun1 of base"<<endl;
+//     }
+// };
+// class Derived: public Base {
+//     public:
+//     void fun2() {
+//         cout<<"fun2 of derived"<<endl;
+//     }
+// };
+// int main() {
+//
+//     Derived d; //fun1 of base fun2 of derived
+//     d.fun1();
+//     d.fun2();
+//
+//     Derived d1; //fun1 of base fun2 of derived
+//     Base *ptr = &d1;
+//     ptr->fun1();
+//     //ptr->fun2(); //den to blepei
+//
+//     Base b;
+//     //Derived *ptr2 = &b; //den epitepetai
+//
+//     return 0;
+// }
