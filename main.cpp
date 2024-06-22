2a5a525eff77088654a98bef55d550b3bd885af1
kyriakidisdimitrios <kyriakidis.demetrios@gmail.com>
2024-06-22 11:21:02 +0100

    feat: fresh project-1 code

--- main.cpp
+++ main.cpp
@@ -3,58 +3,26 @@
 #include <string.h>
 using namespace std;
 
-class Base {
+class Car {
 public:
-    //void fun() {
-    virtual void fun() {
-        cout<<"fun of base"<<endl;
+    virtual void start() {
+        cout<<"Car started"<<endl;
+    }
+    //virtual void start()=0;
+};
+class Innova:public Car {
+    void start() {
+        cout<<"Innova started"<<endl;
     }
 };
-class Derived: public Base {
-public:
-    void fun() {
-        cout<<"fun of derived"<<endl;
+class Swift:public Car {
+    void start() {
+        cout<<"Swift started"<<endl;
     }
 };
 int main() {
-
-    Derived d;
-    //d.fun();
-
-    //Base *ptr = &d;
-    Base *ptr = new Derived();
-    ptr->fun();
-
-
-    return 0;
+    Car *p=new Innova();
+    p->start();
+    p=new Swift();
+    p->start();
 }
-
-
-// class Base {
-// public:
-//     void fun1() {
-//         cout<<"fun1 of base"<<endl;
-//     }
-// };
-// class Derived: public Base {
-//     public:
-//     void fun2() {
-//         cout<<"fun2 of derived"<<endl;
-//     }
-// };
-// int main() {
-//
-//     Derived d; //fun1 of base fun2 of derived
-//     d.fun1();
-//     d.fun2();
-//
-//     Derived d1; //fun1 of base fun2 of derived
-//     Base *ptr = &d1;
-//     ptr->fun1();
-//     //ptr->fun2(); //den to blepei
-//
-//     Base b;
-//     //Derived *ptr2 = &b; //den epitepetai
-//
-//     return 0;
-// }
