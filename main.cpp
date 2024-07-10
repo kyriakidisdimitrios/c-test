f85151b228db7e800eb000124d9d7991b2abcada
kyriakidisdimitrios <kyriakidis.demetrios@gmail.com>
2024-07-10 13:02:57 +0300

    feat: fresh project-1 code

--- main.cpp
+++ main.cpp
@@ -1,30 +1,60 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-class Base {
-public:
-    Base() {
-        cout<<"Constructor of Base"<<endl;
-    }
-    virtual ~Base() { //polu shmantikh h virtual brosta sthn ~Base()
-        cout<<"Destructor of Base"<<endl;
-    }
-};
-class Derived: public Base {
-public:
-    Derived() {
-        cout<<"Constructor of Derived"<<endl;
-    }
-    ~Derived() {
-        cout<<"Destructor of Derived"<<endl;
-    }
-};
-void fun() {
-    Base *p = new Derived();
-    delete p; //prepei na to sbhsw giati brisketai sth Heap
-    //Derived d;
 
+//serialization
+class Student {
+public:
+    string name;
+    int roll;
+    string branch;
+    friend ofstream & operator<<(ofstream &ofs, Student &s);
+    friend ifstream & operator>>(ifstream &ifs, Student &s);
 };
+ifstream & operator>>(ifstream &ifs, Student &s) { //to object Student &s prepei na einai reference alliws den 8a ginetai updated
+    ifs>>s.name>>s.roll>>s.branch;
+    return ifs;
+}
+ofstream & operator<<(ofstream &ofs, Student &s) {
+    ofs<<s.name<<endl;
+    ofs<<s.roll<<endl;
+    ofs<<s.branch<<endl;
+    return ofs;
+}
 int main() {
-    fun();
+    Student s1;
+    s1.name = "John";s1.branch="CS";
+    ofstream ofs("Student.txt", ios::trunc );
+    // ofs<<s1.name<<endl;
+    // ofs<<s1.roll<<endl;
+    // ofs<<s1.branch<<endl;
+
+    //ofs<<s1; //auto den ginetai
+
+    ofs<<s1;
+    ofs.close();
+
+    ifstream
+
+    // ofstream ofs("My.txt", ios::trunc);
+    // ofs<<"John"<<endl;
+    // ofs<<23<<endl;
+    // ofs<<"cs"<<endl;
+    // ofs.close();
+
+    //ifstream ifs("My.txt");
+    // ifstream ifs;
+    // ifs.open("My.txt");
+    // //if (ifs)cout<<"File is opened"<<endl;
+    // if (ifs.is_open())cout<<"File is opened"<<endl;
+    // string name;
+    // int roll;
+    // string branch;
+    // ifs>>name>>roll>>branch;
+    // ifs.close();
+    // cout<<"Name  "<<name<<endl;
+    // cout<<"Roll  "<<roll<<endl;
+    // cout<<"Branch  "<<branch<<endl;
+
 };
