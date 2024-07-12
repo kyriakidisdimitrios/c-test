415248a86d155f4565e7ec5f46e80be132067083
kyriakidisdimitrios <kyriakidis.demetrios@gmail.com>
2024-07-12 11:47:20 +0300

    feat: fresh project-1 code

--- main.cpp
+++ main.cpp
@@ -1,40 +1,21 @@
 #include <iostream>
-#include <vector>
-#include <list>
-#include <forward_list>
-#include <set>
+#include <map>
 using namespace std;
 
 
 int main() {
-    //vector <int> v={2,4,6,8,10};
-    //list <int> v={2,4,6,8,10};
-    //forward_list <int> v={2,4,6,8,10};
-    set <int> v={2,4,6,8,10};
+    map <int, string> m;
+    m.insert(pair<int,string>(1,"john"));
+    m.insert(pair<int,string>(2,"Ravi"));
+    m.insert(pair<int,string>(3,"Khan"));
 
-    // v.push_back(20);
-    // v.push_back(30);
-    // v.pop_back();
-    // v.push_front(20);
-    // v.push_front(30);
-    // v.pop_front();
-    v.insert(20);
-    v.insert(30);
+    map<int, string>::iterator itr;
+    for (itr=m.begin();itr!=m.end();itr++) {
+        cout<<itr->first<<" "<<itr->second<<endl;
+    }
 
-    //vector <int>::iterator itr; //itr pointer
-    //list <int>::iterator itr; //itr pointer
-    //forward_list <int>::iterator itr; //itr pointer
-    set <int>::iterator itr; //itr pointer
-    cout<<"using iterator"<<endl;
-    for(itr = v.begin();itr!=v.end();itr++)
-        cout<<*itr<<endl; //bazei thn timh
-        //cout<<++*itr<<endl; //auksanei thn timh kata 1
-        //sthn set den mporw na allaksw tis times
-
-        //oi times allazoun monima
-
-    cout<<"using for each loop"<<endl;
-    for (int x:v)
-        cout<<x<<endl;
-
+    map<int,string>::iterator itr1;
+    itr1=m.find(2);
+    cout<<"value found is"<<endl;
+    cout<<itr1->first<<" "<<itr1->second<<endl;
 };
