7eebad83551ee89cff0f614f3154c20abf4e65c3
kyriakidisdimitrios <kyriakidis.demetrios@gmail.com>
2024-07-12 17:31:38 +0300

    feat: fresh project-1 code

--- main.cpp
+++ main.cpp
@@ -1,32 +1,47 @@
+#include <cstdarg>
 #include <iostream>
-#include <vector>
-#include <fstream>
+#include <memory>
+
 using namespace std;
 
-template <typename T>
-void fun(T p) {
-    p();
+int sum(int n, ...) {
+    va_list list;
+    va_start(list, n); //n posa arguments einai
+    int x;
+    int s=0;
+
+    for (int i=0;i<n;i++) {
+        x=va_arg(list, int);
+        s+=x;
+    }
+    return s;
 }
-int main(){
-    [](){cout<<"Hello"<<endl;}();
-    [](int x, int y){cout<<"Sum is "<<x+y<<endl;}(10,30);
-    cout<<([](int x, int y){return x+y;}(10,30));
-    int a=[](int x, int y){return x+y;}(10,30);
-    cout<<a;
+int main() {
+    cout<<sum(3,10,20,30)<<endl;
+}
 
-    int b=10;
-    [b](){cout<<b<<endl;}();
-
-    // auto f=[b](){cout<<b<<endl;};
-    // f();
-    // b++;
-    // f();
-    // auto fr=[&b](){cout<<b<<endl;};
-    // fr();
-    // b++;
-    // fr(); //auksanei giati einai reference
-
-    auto f=[&a](){cout<<a++<<endl;};
-    fun(f);
-    fun(f);
-};
+// class Rectangle {
+//     int length;
+//     int breadth;
+// public:
+//     Rectangle(int l, int b) {
+//         length=l;
+//         breadth=b;
+//     }
+//     int area() {
+//         return length*breadth;
+//     }
+//
+// };
+// int main(){
+//     unique_ptr<Rectangle> ptr(new Rectangle(10,5)); //to new to kanei sto Heap
+//     cout<<ptr->area();
+//     unique_ptr<Rectangle> ptr2;
+//     ptr2=move(ptr);
+//     cout<<ptr2->area();
+//     //cout<<ptr->area(); //den ginetai
+//
+//     shared_ptr<Rectangle> ptr3(new Rectangle(10,5));
+//     cout<<ptr3.use_count()<<endl;
+//
+// };
